14c91c9cdc1b91b56a4a9132808db717cf3c5fc7
ghaydaabdoor <ghaydaa.bdoor@outlook.com>
2024-06-27 16:02:22 +0300

    third task

--- 26-6-2024/26-6-2024/Program.cs
+++ 26-6-2024/26-6-2024/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Net.NetworkInformation;
-using System.Xml.Linq;
 
 namespace _26_6_2024
 {
@@ -14,27 +12,19 @@ namespace _26_6_2024
             Console.Write("First Integer: ");
             int firstInt = Convert.ToInt32(Console.ReadLine());
             Console.Write("Second Integer: ");
-            int secondInt= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"The smaller integer is {Math.Min(firstInt,secondInt)}");
+            int secondInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The smaller integer is {Math.Min(firstInt, secondInt)}");
             Console.WriteLine("--------------------");
 
             // Exercise 2
             // Write a C# conditional statement to find the sign  of input  number. Display sign. Go 
             Console.WriteLine("Exercise 2");
-            Console.Write("Enter a number to determine its sign: ");
-            int num= Convert.ToInt32(Console.ReadLine());
-            int signNumber=Math.Sign(num);
-            if (signNumber == -1)
-            {
-                Console.WriteLine($"The sign is -");
-            }
-            else if (signNumber == 1)
-            {
-                Console.WriteLine($"The sign is +");
-            }
-            else if (signNumber == 0) {
-                Console.WriteLine($"The number is zero");
-            }
+            int num3 = 7;
+            int num4 = -24;
+            string sign3 = num3.ToString("+#;-#;0");
+            string sign4 = num4.ToString("+#;-#;0");
+            Console.WriteLine("the sign of 7 is " + sign3[0]);
+            Console.WriteLine("the sign of -24 is " + sign4[0]);
             Console.WriteLine("--------------------");
 
             // Exercise 3
@@ -42,28 +32,28 @@ namespace _26_6_2024
             Console.WriteLine("Exercise 3");
             Console.WriteLine("write three numbers to sort");
             int[] arr = new int[3];
-            for (int i = 0; i < 3; i++)
-            {
-                Console.Write($"Enter number {i+1}: ");
-                arr[i] = Convert.ToInt32(Console.ReadLine());
-            }
+            Console.Write($"Enter number 1: ");
+            arr[0] = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter number 2: ");
+            arr[1] = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter number 3: ");
+            arr[2] = Convert.ToInt32(Console.ReadLine());
             Array.Sort(arr);
-            Console.Write("The ascending order is: ");
-            for (int i = 2; i >= 0; i--) {
-                Console.Write($"{arr[i]} , ");
-             }
+            Console.Write($"The ascending order is: {arr[2]}, {arr[1]}, {arr[0]}");
             Console.WriteLine("\n--------------------");
 
             // Exercise 4
             // Write a C# conditional statement to find the max of five numbers. Use console to display the result.
             Console.WriteLine("Exercise 4");
             Console.WriteLine("Enter 5 numbers to find the max of them");
-            int [] numbers = new int[5];
-            for (int i = 0; i < 5; i++) {
-                numbers[i]=Convert.ToInt32(Console.ReadLine());
-            }
+            int[] numbers = new int[5];
+            numbers[0] = Convert.ToInt32(Console.ReadLine());
+            numbers[1] = Convert.ToInt32(Console.ReadLine());
+            numbers[2] = Convert.ToInt32(Console.ReadLine());
+            numbers[3] = Convert.ToInt32(Console.ReadLine());
+            numbers[4] = Convert.ToInt32(Console.ReadLine());
             Array.Sort(numbers);
-            int maxNumber= numbers[numbers.Length-1];
+            int maxNumber = numbers[numbers.Length - 1];
             Console.WriteLine($"The max number is {maxNumber}");
             Console.WriteLine("--------------------");
 
@@ -96,8 +86,8 @@ namespace _26_6_2024
             //	Input minutes: 546
             //	9 Hours, 6 Minutes
             Console.WriteLine("Exercise 7");
-            Console.WriteLine("Input minutes: ");
-            int mins= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input minutes: ");
+            int mins = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"{mins / 60} Hours, {mins % 60} Minutes");
             Console.WriteLine("--------------------");
 
@@ -106,11 +96,17 @@ namespace _26_6_2024
             // Print all elements with fixed length for each sentence.
             Console.WriteLine("Exercise 8");
             string[] sentences = { "Orange Academy", "location in Irbid", "6 months", "full stack development", "one month internsip" };
-            for (int i = 0; i < sentences.Length; i++)
-            {
-                Console.WriteLine($"{sentences[i]} ==> Length: {sentences[i].Length}");
-            }
-
+            Console.WriteLine($"{sentences[0]} ==> Length: {sentences[0].Length}");
+            Console.WriteLine($"{sentences[1]} ==> Length: {sentences[1].Length}");
+            Console.WriteLine($"{sentences[2]} ==> Length: {sentences[2].Length}");
+            Console.WriteLine($"{sentences[3]} ==> Length: {sentences[3].Length}");
+            Console.WriteLine($"{sentences[4]} ==> Length: {sentences[4].Length}");
         }
     }
 }
+
+
+
+
+
+
---
+++ 27-6/27-6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
